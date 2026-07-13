8c638a2415d3a6b47b93c1918356ee5785fc7bc8
AvidDavid23579 <aviddavid23579@gmail.com>
2026-07-13 21:14:49 +0700

    Fixed trapezoidal profile bug and WIP scurve

--- custom/planning.hpp
+++ custom/planning.hpp
@@ -1,25 +1,24 @@
 #pragma once
 #include <vector>
+
 #include "const.hpp"
 
-struct ProfileConstraints
-{
+struct TrapezoidalProfileConstraints {
     double maxVelocity;
     double maxAcceleration;
 };
 
-struct ProfileState
-{
+struct TrapezoidalProfileState {
     double position;
     double velocity;
+    double acceleration;
 };
 
-class TrapezoidalProfile
-{
-  private:
-    ProfileConstraints m_constraints;
-    ProfileState m_initial{};
-    ProfileState m_goal{};
+class TrapezoidalProfile {
+   private:
+    TrapezoidalProfileConstraints m_constraints;
+    TrapezoidalProfileState m_initial{};
+    TrapezoidalProfileState m_goal{};
 
     double m_direction = 1.0;
     double m_t_accel = 0.0;
@@ -28,13 +27,60 @@ class TrapezoidalProfile
     double m_totalTime = 0.0;
     bool m_generated = false;
 
-  public:
-    TrapezoidalProfile(ProfileConstraints constraints);
+   public:
+    TrapezoidalProfile(TrapezoidalProfileConstraints constraints);
 
-    void generate(ProfileState initial, ProfileState goal);
-    ProfileState sample(double t);
+    void generate(TrapezoidalProfileState initial, TrapezoidalProfileState goal);
+    TrapezoidalProfileState sample(double t);
     bool isFinished(double t);
     double totalTime();
+};
+
+struct SCurveConstraints {
+    double maxVelocity;
+    double maxAcceleration;
+    double maxJerk;
+};
+
+struct SCurveState {
+    double position;
+    double velocity;
+    double acceleration;
+    double jerk;
+};
+
+struct SCurveSegment {
+    double t_start;
+    double duration;
+
+    double p0;
+    double v0;
+    double a0;
+
+    double jerk;
+};
+
+class SCurveProfile {
+   private:
+    SCurveConstraints m_constraints;
+    double m_time_jerk;
+    double m_time_accel;
+    double m_time_cruise;
+
+    double m_a_peak;
+    double m_v_peak;
+
+    double m_total_time;
+    SCurveSegment m_segments;
+
+    double m_direction = 1.0;
+
+   public:
+    SCurveProfile(SCurveConstraints constraints);
+
+    void generate(SCurveState initial, SCurveState goal);
+    SCurveSegment build_segments();
+
 };
 
 /*
@@ -43,54 +89,29 @@ Path Planning Class
 
 */
 
-class CubicHermitePathPlanner
-{
-  private:
+class CubicHermitePathPlanner {
+   private:
     std::vector<WaypointVel> m_waypoints;
 
     // Cubic Hermite basis functions
-    static double h00(double t)
-    {
-        return 2 * t * t * t - 3 * t * t + 1;
-    }
-    static double h10(double t)
-    {
-        return t * t * t - 2 * t * t + t;
-    }
-    static double h01(double t)
-    {
-        return -2 * t * t * t + 3 * t * t;
-    }
-    static double h11(double t)
-    {
-        return t * t * t - t * t;
-    }
+    static double h00(double t) { return 2 * t * t * t - 3 * t * t + 1; }
+    static double h10(double t) { return t * t * t - 2 * t * t + t; }
+    static double h01(double t) { return -2 * t * t * t + 3 * t * t; }
+    static double h11(double t) { return t * t * t - t * t; }
 
     // Derivatives (for heading)
-    static double dh00(double t)
-    {
-        return 6 * t * t - 6 * t;
-    }
-    static double dh10(double t)
-    {
-        return 3 * t * t - 4 * t + 1;
-    }
-    static double dh01(double t)
-    {
-        return -6 * t * t + 6 * t;
-    }
-    static double dh11(double t)
-    {
-        return 3 * t * t - 2 * t;
-    }
+    static double dh00(double t) { return 6 * t * t - 6 * t; }
+    static double dh10(double t) { return 3 * t * t - 4 * t + 1; }
+    static double dh01(double t) { return -6 * t * t + 6 * t; }
+    static double dh11(double t) { return 3 * t * t - 2 * t; }
 
     Pose sampleSegment(int i, double t) const;
 
-  public:
-  CubicHermitePathPlanner();
+   public:
+    CubicHermitePathPlanner();
 
-  void addWaypoint(const WaypointVel& wp);
-  void clearWaypoints();
-  WaypointVel fromHeading(double x, double y, double heading, double speed = 1.0);
-  std::vector<Pose> generate(int samplesPerSegment = 50) const;
+    void addWaypoint(const WaypointVel& wp);
+    void clearWaypoints();
+    WaypointVel fromHeading(double x, double y, double heading, double speed = 1.0);
+    std::vector<Pose> generate(int samplesPerSegment = 50) const;
 };
--- src/Framework/planning.cpp
+++ src/Framework/planning.cpp
@@ -1,12 +1,13 @@
 #include "planning.hpp"
-#include <cmath>
+
 #include <algorithm>
+#include <cmath>
 #include <stdexcept>
 
-TrapezoidalProfile::TrapezoidalProfile(ProfileConstraints constraints) : m_constraints(constraints) {}
+TrapezoidalProfile::TrapezoidalProfile(TrapezoidalProfileConstraints constraints)
+    : m_constraints(constraints) {}
 
-void TrapezoidalProfile::generate(ProfileState initial, ProfileState goal)
-{
+void TrapezoidalProfile::generate(TrapezoidalProfileState initial, TrapezoidalProfileState goal) {
     m_initial = initial;
     m_goal = goal;
 
@@ -28,17 +29,14 @@ void TrapezoidalProfile::generate(ProfileState initial, ProfileState goal)
 
     double d_cruise = dist - d_accel - d_decel;
 
-    if (d_cruise < 0)
-    {
+    if (d_cruise < 0) {
         double v_peak = std::sqrt((2.0 * a * dist + v0 * v0 + vf * vf) / 2.0);
         v_peak = std::min(v_peak, vMax);
 
         m_t_accel = (v_peak - v0) / a;
         m_t_decel = (v_peak - vf) / a;
         m_t_cruise = 0.0;
-    }
-    else
-    {
+    } else {
         m_t_cruise = d_cruise / vMax;
     }
 
@@ -46,10 +44,8 @@ void TrapezoidalProfile::generate(ProfileState initial, ProfileState goal)
     m_generated = true;
 }
 
-ProfileState TrapezoidalProfile::sample(double t)
-{
-    if (!m_generated)
-        return m_initial;
+TrapezoidalProfileState TrapezoidalProfile::sample(double t) {
+    if (!m_generated) return m_initial;
 
     t = std::clamp(t, 0.0, m_totalTime);
 
@@ -60,22 +56,17 @@ ProfileState TrapezoidalProfile::sample(double t)
 
     double pos, vel;
 
-    if (t <= m_t_accel)
-    {
+    if (t <= m_t_accel) {
         vel = v0 + a * t;
         pos = v0 * t + 0.5 * a * t * t;
-    }
-    else if (t <= m_t_accel + m_t_cruise)
-    {
+    } else if (t <= m_t_accel + m_t_cruise) {
         double dt = t - m_t_accel;
-        double d_accel = v0 * m_t_accel + 0.5 * m_t_accel * m_t_accel;
+        double d_accel = v0 * m_t_accel + 0.5 * a * m_t_accel * m_t_accel;
         vel = v_peak;
         pos = d_accel + v_peak * dt;
-    }
-    else
-    {
+    } else {
         double dt = t - m_t_accel - m_t_cruise;
-        double d_accel = v0 * m_t_accel + 0.5 * m_t_accel * m_t_accel;
+        double d_accel = v0 * m_t_accel + 0.5 * a * m_t_accel * m_t_accel;
         double d_cruise = v_peak * m_t_cruise;
         vel = v_peak - a * dt;
         pos = d_accel + d_cruise + v_peak * dt - 0.5 * a * dt * dt;
@@ -83,14 +74,65 @@ ProfileState TrapezoidalProfile::sample(double t)
     return {m_initial.position + m_direction * pos, m_direction * vel};
 }
 
-bool TrapezoidalProfile::isFinished(double t)
-{
-    return m_generated && t >= m_totalTime;
-}
+bool TrapezoidalProfile::isFinished(double t) { return m_generated && t >= m_totalTime; }
 
-double TrapezoidalProfile::totalTime()
-{
-    return m_totalTime;
+double TrapezoidalProfile::totalTime() { return m_totalTime; }
+
+SCurveProfile::SCurveProfile(SCurveConstraints constraints) : m_constraints(constraints) {}
+
+void SCurveProfile::generate(SCurveState initial, SCurveState goal) {
+    double delta_pos = goal.position - initial.position;
+    m_direction = (delta_pos >= 0) ? 1.0 : -1.0;
+    double distance = std::abs(delta_pos);
+
+    if (distance == 0) {
+        m_time_jerk = m_time_accel = m_time_cruise = 0;
+        m_a_peak = 0.0;
+        m_v_peak = 0.0;
+        m_total_time = 0.0;
+        m_segments = {};
+    }
+
+    double time_jerk_full = m_constraints.maxAcceleration / m_constraints.maxJerk;
+
+    if (m_constraints.maxAcceleration * time_jerk_full > m_constraints.maxVelocity) {
+        m_time_jerk = sqrt(m_constraints.maxVelocity / m_constraints.maxJerk);
+        m_time_accel = 0.0;
+        m_a_peak = m_constraints.maxJerk * m_time_jerk;
+    } else {
+        m_time_jerk = time_jerk_full;
+        m_time_accel = m_constraints.maxVelocity - m_constraints.maxAcceleration - m_time_jerk;
+        m_a_peak = m_constraints.maxAcceleration;
+    }
+
+    m_v_peak = m_constraints.maxVelocity;
+    double time_ramp = 2 * m_time_jerk + m_time_accel;
+    double distance_ramp = m_v_peak * time_ramp;
+
+    if (distance_ramp <= distance) {
+        m_time_cruise = (distance - distance_ramp) / m_v_peak;
+    } else {
+        m_time_cruise = 0.0;
+        double b = m_constraints.maxAcceleration * time_jerk_full;
+        double v_candidate =
+            (-b + sqrt(pow(b, 2)) + 4 * m_constraints.maxAcceleration * distance) / 2;
+
+        if (v_candidate >= m_constraints.maxAcceleration * time_jerk_full) {
+            m_v_peak = v_candidate;
+            m_time_jerk = time_jerk_full;
+            m_time_accel = m_v_peak / m_constraints.maxAcceleration - m_time_jerk;
+            m_a_peak = m_constraints.maxAcceleration;
+        } else {
+            m_v_peak = pow((distance * sqrt(m_constraints.maxJerk) / 2), (2.0 / 3.0));
+            m_time_jerk = sqrt(m_v_peak / m_constraints.maxJerk);
+            m_time_accel = 0.0;
+            m_a_peak = m_constraints.maxJerk * time_jerk_full;
+        }
+
+        m_total_time = 4 * m_time_jerk + 2 * m_time_accel + m_time_cruise;
+
+        SCurveProfile::build_segments();
+    }
 }
 
 /*
@@ -100,8 +142,7 @@ Path Planning Class
 */
 CubicHermitePathPlanner::CubicHermitePathPlanner() {}
 
-Pose CubicHermitePathPlanner::sampleSegment(int i, double t) const
-{
+Pose CubicHermitePathPlanner::sampleSegment(int i, double t) const {
     const WaypointVel& p0 = m_waypoints[i];
     const WaypointVel& p1 = m_waypoints[i + 1];
 
@@ -115,35 +156,24 @@ Pose CubicHermitePathPlanner::sampleSegment(int i, double t) const
     return {x, y, heading};
 }
 
-void CubicHermitePathPlanner::addWaypoint(const WaypointVel& wp)
-{
-    m_waypoints.push_back(wp);
-}
+void CubicHermitePathPlanner::addWaypoint(const WaypointVel& wp) { m_waypoints.push_back(wp); }
 
-void CubicHermitePathPlanner::clearWaypoints()
-{
-    m_waypoints.clear();
-}
+void CubicHermitePathPlanner::clearWaypoints() { m_waypoints.clear(); }
 
-WaypointVel CubicHermitePathPlanner::fromHeading(double x, double y, double heading, double speed)
-{
+WaypointVel CubicHermitePathPlanner::fromHeading(double x, double y, double heading, double speed) {
     return {{x, y, heading}, cos(heading) * speed, sin(heading) * speed};
 }
 
-std::vector<Pose> CubicHermitePathPlanner::generate(int samplesPerSegment) const
-{
-    if (m_waypoints.size() < 2)
-        throw std::runtime_error("Need at least 2 waypoints");
+std::vector<Pose> CubicHermitePathPlanner::generate(int samplesPerSegment) const {
+    if (m_waypoints.size() < 2) throw std::runtime_error("Need at least 2 waypoints");
 
     std::vector<Pose> path;
-    
+
     int numSegments = static_cast<int>(m_waypoints.size()) - 1;
     path.reserve(numSegments * samplesPerSegment + 1);
 
-    for (int i = 0; i < numSegments; ++i)
-    {
-        for (int s = 0; s < samplesPerSegment; ++s)
-        {
+    for (int i = 0; i < numSegments; ++i) {
+        for (int s = 0; s < samplesPerSegment; ++s) {
             double t = static_cast<double>(s) / samplesPerSegment;
             path.push_back(sampleSegment(i, t));
         }
